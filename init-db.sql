da6e379873b761f1c9477b5379638d8c57fc0dc8
Lê Thanh Nhàn <156575614+iuh21000281@users.noreply.github.com>
2024-03-28 15:36:33 +0700

    init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS tblbookIUHX (
 );
 
 INSERT INTO tblbook (title, author) VALUES
-  ('DevOps', 'IUHX'),
-  ('Big Data', 'IUHX'),
-  ('Cloud Deployement', 'IUHX'),
-('Data Analysis', 'IUHX'),
-('Block Chain', 'IUHX');
+  ('DevOps', 'IUH21000281'),
+  ('Big Data', 'IUH21000281'),
+  ('Cloud Deployement', 'IUH21000281'),
+('Data Analysis', 'IUH21000281'),
+('Block Chain', 'IUH21000281');
